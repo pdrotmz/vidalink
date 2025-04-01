5400a52c3adc1a514d9191142673e78098f37798
IanCaleb <ivusaw1227@gmail.com>
2025-04-01 19:13:31 -0300

    feat(db): adiciona a estrutura inicial da modelagem do banco de dados

---
+++ DataBase/db_doacao.sql
@@ -0,0 +1,87 @@
+-- --------------------------------------------------------
+-- Servidor:                     127.0.0.1
+-- Versão do servidor:           8.3.0 - MySQL Community Server - GPL
+-- OS do Servidor:               Win64
+-- HeidiSQL Versão:              12.10.0.7000
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Copiando estrutura do banco de dados para doacaosangue
+CREATE DATABASE IF NOT EXISTS `doacaosangue` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `doacaosangue`;
+
+-- Copiando estrutura para tabela doacaosangue.adiministrador
+CREATE TABLE IF NOT EXISTS `adiministrador` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(50) NOT NULL DEFAULT '0',
+  `email` varchar(50) NOT NULL DEFAULT '0',
+  `senha` varchar(50) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Exportação de dados foi desmarcado.
+
+-- Copiando estrutura para tabela doacaosangue.comprovante
+CREATE TABLE IF NOT EXISTS `comprovante` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `data` date NOT NULL DEFAULT '0000-00-00',
+  `usuarioDoador` int NOT NULL DEFAULT (0),
+  `usuarioIndicador` int NOT NULL DEFAULT (0),
+  `status` varchar(50) NOT NULL DEFAULT 'invalida',
+  PRIMARY KEY (`id`),
+  KEY `FK__usuarioDoador` (`usuarioDoador`) USING BTREE,
+  KEY `FK__usuarioIndicador` (`usuarioIndicador`) USING BTREE
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Exportação de dados foi desmarcado.
+
+-- Copiando estrutura para tabela doacaosangue.perfil
+CREATE TABLE IF NOT EXISTS `perfil` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `usuario` int NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `FK__usuario` (`usuario`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Exportação de dados foi desmarcado.
+
+-- Copiando estrutura para tabela doacaosangue.recompensa
+CREATE TABLE IF NOT EXISTS `recompensa` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(50) NOT NULL DEFAULT '0',
+  `descricao` mediumtext NOT NULL,
+  `custo` int NOT NULL DEFAULT (0),
+  `imagem` int NOT NULL DEFAULT (0),
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Exportação de dados foi desmarcado.
+
+-- Copiando estrutura para tabela doacaosangue.usuario
+CREATE TABLE IF NOT EXISTS `usuario` (
+  `Id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(50) NOT NULL DEFAULT '0',
+  `email` varchar(50) NOT NULL DEFAULT '0',
+  `senha` varchar(50) NOT NULL DEFAULT '0',
+  `pontos` int NOT NULL DEFAULT (0),
+  `codigoDeRecomendacao` int NOT NULL DEFAULT (0),
+  `foto` int DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Exportação de dados foi desmarcado.
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
