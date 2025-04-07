68b347805ca655ae96e06cbad4bb3793e44580c0
ghesse-p <ghessenonato@aluno.unifapce.edu.br>
2025-04-07 18:45:12 -0300

    Adiciona telas Sobre e Contato

--- src/main/frontend/src/App.jsx
+++ src/main/frontend/src/App.jsx
@@ -1,16 +1,22 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import '../src/styles/globals.css'
-import { HomePage } from './pages/HomePage'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "../src/styles/globals.css";
+import { HomePage } from "./pages/HomePage";
+import { SobrePage } from "./pages/SobrePage";
+import ContatoPage from "./pages/ContatoPage";
+
 
 function App() {
-
+  
   return (
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<HomePage />} />
-        </Routes>
-      </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/sobre" element={<SobrePage />} />
+        <Route path="/contato" element={<ContatoPage />} />
+
+      </Routes>
+    </BrowserRouter>
   );
 }
 
-export default App
+export default App;
