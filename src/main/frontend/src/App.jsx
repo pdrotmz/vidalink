28448496449c7f9df19222e9dba8ecb9835a9acf
pdrotmz <pedrotomaz0210@gmail.com>
2025-04-08 07:44:19 -0300

    fix: ajusta bug de import e renderização das páginas

--- src/main/frontend/src/App.jsx
+++ src/main/frontend/src/App.jsx
@@ -1,19 +1,20 @@
+import '@fontsource/montserrat/400.css';
+import '@fontsource/montserrat/600.css';
+import '@fontsource/montserrat/700.css';
+
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "../src/styles/globals.css";
-import { HomePage } from "./pages/HomePage";
-import { SobrePage } from "./pages/SobrePage";
-import ContatoPage from "./pages/ContatoPage";
-
+import HomePage from "../src/pages/HomePage";
+import SobrePage from "../src/pages/SobrePage";
+import ContatoPage from "../src/pages/ContatoPage";
 
 function App() {
-  
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<HomePage />} index />
         <Route path="/sobre" element={<SobrePage />} />
         <Route path="/contato" element={<ContatoPage />} />
-
       </Routes>
     </BrowserRouter>
   );
