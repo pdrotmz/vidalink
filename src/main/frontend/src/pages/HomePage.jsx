efd092cd75b077c19ee0bf667a0659cea5a9ab15
pdrotmz <pedrotomaz0210@gmail.com>
2025-04-08 16:35:18 -0300

    feat: ajusta css a HomePage

--- src/main/frontend/src/pages/HomePage.jsx
+++ src/main/frontend/src/pages/HomePage.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import '../styles/HomePage.css';
+import bloodDrop from '../assets/images/blood-drop.png'
+import vidalink from '../assets/images/logo-vidalink.png'
 
 const HomePage = () => {
   return (
@@ -18,15 +20,24 @@ const HomePage = () => {
       </div>
      </div>
       
-      <main className="main-content">
+      <main className='main-content'>
         <div className='main-text'>
           <h1>Doe sangue, <br /> Salve vidas</h1>
+         <div className='blood-drop'>
+          <img src={bloodDrop} alt="Gota de sangue" />
+         </div>
         </div>
-        <div>
-          <p>Sua doação pode trazer esperança e cura para aqueles que precisam.</p>
+
+        <div className='call-action'>
+         <p>Sua doação pode trazer esperança e cura para aqueles que precisam.</p>
           <button className="donate-button">Doar agora</button>
         </div>
+        <div className='logo'>
+          <img src={vidalink} alt="Logo da Vidalink" />
+        </div>
       </main>
+
+
     </div>
   );
 };
