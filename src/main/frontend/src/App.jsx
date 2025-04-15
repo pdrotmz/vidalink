a76e897b33dcec6fe96d51efb1b2cd4df3693751
pdrotmz <pedrotomaz0210@gmail.com>
2025-04-15 16:46:49 -0300

    feat: atualiza as rotas
    - Adiciona rota de DoarPage
    - Adicionar rota de ContatoPage
    - Adicionar rota de SobrePage

--- src/main/frontend/src/App.jsx
+++ src/main/frontend/src/App.jsx
@@ -1,42 +1,24 @@
-<<<<<<< HEAD
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import '../src/styles/globals.css'
 import { HomePage } from './pages/HomePage'
 import { CadastroProdutos } from './pages/CadastroProdutos';
 import { ListagemProdutos } from './pages/ListagemProdutos';
-=======
-import '@fontsource/montserrat/400.css';
-import '@fontsource/montserrat/600.css';
-import '@fontsource/montserrat/700.css';
-
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "../src/styles/globals.css";
-import HomePage from "../src/pages/HomePage";
-import SobrePage from "../src/pages/SobrePage";
-import ContatoPage from "../src/pages/ContatoPage";
-import DoarPage from "../src/pages/DoarPage";
->>>>>>> develop
+import { SobrePage } from './pages/SobrePage';
+import { DoarPage } from './pages/DoarPage';
+import { ContatoPage } from './pages/ContatoPage';
 
 function App() {
   return (
-<<<<<<< HEAD
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<HomePage />} />
           <Route path='/CadastroProdutos' element={<CadastroProdutos />} />
           <Route path='/ListagemProdutos' element={<ListagemProdutos />} />
+          <Route path='SobrePage' element={<SobrePage />} />
+          <Route path='DoarPage' element={<DoarPage />} />
+          <Route path='ContatoPage' element={<ContatoPage />} />
         </Routes>
       </BrowserRouter>
-=======
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} index />
-        <Route path="/sobre" element={<SobrePage />} />
-        <Route path="/contato" element={<ContatoPage />} />
-        <Route path="/doar" element={<DoarPage />} />
-      </Routes>
-    </BrowserRouter>
->>>>>>> develop
   );
 }
 
