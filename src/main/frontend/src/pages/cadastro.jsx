a2cdc6f820bef8d5e9e7b82085d635530b70d4c7
_jgabrie.l <joaogabriel150234@gmail.com>
2025-05-06 16:43:51 -0300

    CADASTRO

--- src/main/frontend/src/pages/cadastro.jsx
+++ src/main/frontend/src/pages/cadastro.jsx
@@ -1,13 +1,32 @@
-import React from "react";
-import { Header } from "../components/ui/Header";
-import "../styles/CadastroProdutos.css"
+import React from 'react';
+import '../assets/cadastro.css'; 
 
-export const Cadastro = () => {
-
+export default function Cadastro() {
     return (
-        <div className="">
-            
+        <div className="page">
+            <h2 className="titulo">
+                Salve vidas com um simples gesto! Cadastre-se<br />
+                e faça a diferença na vida de quem precisa.
+            </h2>
+
+            <div className="container">
+                <form>
+                    <div className="form-group">
+                        <input type="text" placeholder="Nome Completo" required />
+                    </div>
+                    <div className="form-group">
+                        <input type="email" placeholder="Email" required />
+                    </div>
+                    <div className="form-group">
+                        <input type="password" placeholder="Senha" required />
+                    </div>
+                    <p className="terms">
+                        <img src="imagens/question.png" alt="" className="terms-icone" />
+                        Ao clicar no botão cadastre-se você aceita os <strong>Termos de Uso e Privacidade</strong>
+                    </p>
+                    <button className="button" type="submit">CADASTRAR</button>
+                </form>
+            </div>
         </div>
-       
     );
 }
