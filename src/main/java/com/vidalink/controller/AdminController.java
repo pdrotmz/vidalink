7807c7486be034df70a15ca47b784fe52c69103e
_jgabrie.l <joaogabriel150234@gmail.com>
2025-05-20 13:15:57 -0300

    feat(Controller): adicionando AdminController

---
+++ src/main/java/com/vidalink/controller/AdminController.java
@@ -0,0 +1,50 @@
+package com.vidalink.controller;
+
+import com.vidalink.model.user.User;
+import com.vidalink.services.UserService;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/admin/users")
+@CrossOrigin(origins = "http://localhost:3000")
+public class AdminController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        return ResponseEntity.ok(userService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable UUID id) {
+        Optional<User> user = userService.findById(id);
+        return user.map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUserEmail(@PathVariable UUID id, @RequestBody @Valid User updatedUser) {
+        try {
+            User user = userService.updateLabelById(id, updatedUser);
+            return ResponseEntity.ok(user);
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable UUID id) {
+        userService.deleteUserById(id);
+        return ResponseEntity.noContent().build();
+    }
+}
