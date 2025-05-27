432dd3398ffbc3dc7c7066cc73e5625fc943ebc2
IanCaleb <ivusaw1227@gmail.com>
2025-05-27 00:08:09 -0300

    refacture: recria página de Cadastro

--- src/main/frontend/src/pages/cadastro.jsx
+++ src/main/frontend/src/pages/cadastro.jsx
@@ -1,32 +1,50 @@
 import React from 'react';
-import '../Styles/cadastro.css'; 
+import { Link } from 'react-router-dom';
+
+import gif from '../Assets/images/gif_doacao.gif'
+import '../Styles/cadastroUsuarios.css'; 
 
 export default function Cadastro() {
     return (
-        <div className="CadastroContainer">
-            <h2 className="titulo">
-                Salve vidas com um simples gesto! Cadastre-se<br />
-                e faça a diferença na vida de quem precisa.
-            </h2>
+        <div className="containerCadastro">
 
-            <div className="container">
-                <form>
-                    <div className="form-group">
-                        <input type="text" placeholder="Nome Completo" required />
-                    </div>
-                    <div className="form-group">
-                        <input type="email" placeholder="Email" required />
-                    </div>
-                    <div className="form-group">
-                        <input type="password" placeholder="Senha" required />
-                    </div>
-                    <p className="terms">
-                        <img src="imagens/question.png" alt="" className="terms-icone" />
-                        Ao clicar no botão cadastre-se você aceita os <strong>Termos de Uso e Privacidade</strong>
-                    </p>
-                    <button className="button" type="submit">CADASTRAR</button>
-                </form>
+            <div className='cadastroEsquerda'>
+                <img src={gif} alt="" />
+
             </div>
+
+            <div className='cadastroDireita'>
+                <form action="">
+                    <h1>Bem vindo!</h1>
+
+                    <div className='formulario'>
+                        <label htmlFor="">Nome:</label>
+                        <br />
+                        <input type="text" />
+                        <br /><br />
+
+                        <label htmlFor="">E-mail:</label>
+                        <br />
+                        <input type="text" />
+                        <br /><br />
+
+                        <label htmlFor="">Senha:</label>
+                        <br />
+                        <input type="text" />
+                        <br />
+                    </div>
+
+                    <div className='botao'>
+                        <button>Cadastrar</button>
+
+                        <p>Já tem conta? <Link to={"/Login"}>Entrar</Link></p>
+                    </div>
+                    
+                </form>
+                
+
+            </div>
+            
         </div>
     );
 }
