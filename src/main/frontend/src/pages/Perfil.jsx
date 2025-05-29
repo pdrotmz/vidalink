21f3f71519067aa63c1ba68c66b2cf3efc7bb461
IanCaleb <ivusaw1227@gmail.com>
2025-05-29 20:44:04 -0300

    .

--- src/main/frontend/src/pages/Perfil.jsx
+++ src/main/frontend/src/pages/Perfil.jsx
@@ -5,14 +5,14 @@ import { Link } from "react-router-dom";
 import "../Styles/Principal.css"
 
 import Header from "../Components/Header";
+import RecompensaPerfil from "../Components/RecompensaPerfil";
 
+import InfoPerfil from "../Components/InfoPerfil";
 import fotoperfil from "../Assets/images/perfildejhon.jpg"
 
-import certificado1 from "../Assets/images/certificado1.png"
-
-import certificado2 from "../Assets/images/certificado2.png"
-
-import certificado3 from "../Assets/images/certificado3.png"
+import Certificado1 from "../Assets/images/certificado1.png"
+import Certificado2 from "../Assets/images/certificado2.png"
+import Certificado3 from "../Assets/images/certificado3.png"
 
 
 export const Perfil = () => {
@@ -22,7 +22,20 @@ export const Perfil = () => {
             <Header/>
 
             <div className="perfilContent">
-                <div className="perfil"></div>
+                <InfoPerfil/>
+
+                <h1>Certificados:</h1>
+
+                <ul>
+                    <RecompensaPerfil imagem={Certificado1} nome={'Certificado1'} recompensaId={1}/>
+                    <RecompensaPerfil imagem={Certificado2} nome={'Certificado1'} recompensaId={2}/>
+                    <RecompensaPerfil imagem={Certificado3} nome={'Certificado1'} recompensaId={3}/>
+                    <RecompensaPerfil imagem={Certificado1} nome={'Certificado1'} recompensaId={1}/>
+                    <RecompensaPerfil imagem={Certificado1} nome={'Certificado1'} recompensaId={1}/>
+                    <RecompensaPerfil imagem={Certificado1} nome={'Certificado1'} recompensaId={1}/>
+                    
+                    
+                </ul>
             </div>
         </div> 
         
