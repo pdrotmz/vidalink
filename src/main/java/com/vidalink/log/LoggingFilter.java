68bbe626d0059291b12fabb6307d6286a1cef714
pdrotmz <pedrotomaz0210@gmail.com>
2025-06-02 15:32:09 -0300

    feat(logging): add LoggingFilter

---
+++ src/main/java/com/vidalink/log/LoggingFilter.java
@@ -0,0 +1,34 @@
+package com.vidalink.log;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Enumeration;
+
+@Component
+public class LoggingFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+        String url = request.getRequestURL().toString();
+        String query = request.getQueryString();
+
+        System.out.println("Request: " + url + (query != null ? "?" + query : ""));
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String name = headerNames.nextElement();
+            String value = request.getHeader(name);
+            System.out.println("Header: " + name + " = " + value);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
+
