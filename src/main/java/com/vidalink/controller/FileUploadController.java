064bc3b4527e7800d1a788482f3cbf297e1ec05a
pdrotmz <pedrotomaz0210@gmail.com>
2025-06-12 13:39:12 -0300

    fix: adjust image bug on "top medalhas" #8

--- src/main/java/com/vidalink/controller/FileUploadController.java
+++ src/main/java/com/vidalink/controller/FileUploadController.java
@@ -5,10 +5,7 @@ import org.springframework.core.io.UrlResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -19,6 +16,8 @@ import java.nio.file.StandardCopyOption;
 import java.util.Map;
 import java.util.UUID;
 
+@RestController
+@RequestMapping("/api/files")
 public class FileUploadController {
 
 
